93d9a03588d6d9fe071d429ceedde8f0cda99908
uziel@svnb001 <uziel@svnb001>
2022-10-04 13:44:09 -0300

    Dockerfile Node+ARG+ENV+EXPOSE no Docker Alura v2

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+let app = express();
+
+app.use(express.static("."));
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+//app.listen("3000", ()=>{
+app.listen(process.env.PORT, ()=>{
+    console.log("Server is listening on port 3000")
+})
