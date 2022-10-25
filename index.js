0f662254ab7f479495b725c45e2dc7f8964f3907
uziel@svnb001 <uziel@svnb001>
2022-10-25 15:17:07 -0300

    Renomeado Título (App Server 1)

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get("/", (req,res)=>{
 
 //app.listen("3000", ()=>{
 app.listen(process.env.PORT, ()=>{
-    console.log("Server is listening on port 3000")
+    console.log("Server is listening on port 6000")
 })
